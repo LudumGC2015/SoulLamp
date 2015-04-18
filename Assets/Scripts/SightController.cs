e7d4843c9ee77b4327c223dd4ba243037ba6b88c
ClubbingSealCub <danroe@witty.com>
2015-04-18 17:13:51 +0100

    Added enemy patrol movement, player sighting / following under construction.

---
+++ Assets/Scripts/SightController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SightController : MonoBehaviour {
+
+    public PatrolController patrolController;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            patrolController.SetPlayerSighted(true);
+        }
+    }
+}
