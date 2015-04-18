1f658bbd33b09d2f202533c44a576bb33f457f33
ClubbingSealCub <danroe@witty.com>
2015-04-18 21:15:46 +0100

    Implemented soul collection. ImplemenING enemy collisions.

---
+++ Assets/HurtBoxController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurtBoxController : MonoBehaviour {
+
+    public Rigidbody2D player;
+
+    void OnColliderEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.AddForce(new Vector2(-10f, -10f), ForceMode2D.Impulse);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
